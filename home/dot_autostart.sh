c1017788c78ddd240881caef498b8b574d129c5b
taep96 <64481039+taep96@users.noreply.github.com>
2022-05-29 21:14:34 +0200

    initial commit

---
+++ home/dot_autostart.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+~/.fehbg &
+picom &
+dwmblocks &
+discord --enable-gpu-rasterization & # Fix server menu scroll lag
+firefox &
