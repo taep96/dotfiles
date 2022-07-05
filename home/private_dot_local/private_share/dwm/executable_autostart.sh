8158708e7026ac4d2c0f9bd7eb169aa13aa458b7
taep96 <64481039+taep96@users.noreply.github.com>
2022-07-05 02:19:40 +0200

    update

--- home/private_dot_local/private_share/dwm/executable_autostart.sh
+++ home/private_dot_local/private_share/dwm/executable_autostart.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-~/.fehbg &
+feh --no-fehbg --bg-scale '/home/taep96/pictures/wallpapers/monogatari.png' &
 picom &
 dwmblocks &
 discord --enable-gpu-rasterization & # Fix server menu scroll lag
